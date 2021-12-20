77367c898f95116f337bad5216399af542a91967
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-12-20 10:48:05 -0800

    Created basic, unstyled Footer component and imported into App

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="App">
 
       <Header />
+
+      <Footer />
 
     </div>
   );
