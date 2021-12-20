9e8924afdaca951a43176082fa0b82a7efa3e5a1
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-20 13:43:21 -0500

    Initial Commit

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
       <Header />
+      <Router>
+        <Routes>
+          <Route path='/home'/>
+        </Routes>
+        
+      </Router>
+      
 
     </div>
   );
