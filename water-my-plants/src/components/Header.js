3438435b6ef85c5d62ea88a25421be6df1fd0c46
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-12-20 10:12:15 -0800

    Created basic, unstyled header component

---
+++ water-my-plants/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return(
+    <div className='header-container'>
+      <span>Water My Plants</span>
+        <a href='#'>Home</a>
+        <a href='#'>Login</a>
+    </div>
+  )
+}
