9279a20286e7fa8a8b427ea27ad173aef6053a85
Lia Moua <moua0061@yahoo.com>
2021-12-21 13:30:11 -0500

    made a editform component for plant

--- water-my-plants/src/components/MyPlant.js
+++ water-my-plants/src/components/MyPlant.js
@@ -5,12 +5,19 @@ const MyPlant = (props) => {
     // console.log(props)
     const {plant} = props;
 
+    const handleClick = () => {
+
+    }
+
+
+
     return (
         <div>
             <img src={plant.image} alt='plant' width='200px' />
             <h1>{plant.nickname}</h1>
             <p>Species: {plant.species}</p>
             <p>Water Frequency: {plant.h2oFrequency}</p>
+            <button>edit me!</button>
         </div>
     )
 }
