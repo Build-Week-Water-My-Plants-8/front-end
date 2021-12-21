b94eb29977194526b84026c7fcd922d56763f3e6
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-21 12:53:22 -0500

    Prepared code in PrivateRoute for if Redirect no longer functions in v6

--- water-my-plants/src/components/PrivateRoute.js
+++ water-my-plants/src/components/PrivateRoute.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useNavigate } from "react-router-dom";
 
 function PrivateRoute({component: Component, ...rest}) {
+  //const naviate = useNavigate();
   return (<Route {...rest} render={() => {
   if(localStorage.getItem('token')){
     return (<Component/>)
   } else {
-    return <Redirect to={'/login'}/>
+    return <Redirect to={'/login'}/> //navigate('/login');
   }
   }
   }/>);
