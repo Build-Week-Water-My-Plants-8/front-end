3b499cea335c3161019984d8d507798882e5ff7a
Lia Moua <moua0061@yahoo.com>
2021-12-21 23:04:37 -0500

    fixed img size to be responsive

--- water-my-plants/src/components/MyPlant.js
+++ water-my-plants/src/components/MyPlant.js
@@ -14,7 +14,7 @@ const MyPlant = (props) => {
 
     return (
         <div>
-            <img src={plant.image} alt='plant' width='200px' />
+            <img src={plant.image} alt='plant' width='15%' />
             <h1>{plant.nickname}</h1>
             <p>Species: {plant.species}</p>
             <p>Water Frequency: {plant.h2oFrequency}</p>
