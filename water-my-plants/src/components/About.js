2c3ec73c89c47d2682e1455a546e0fa321ca3b3f
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-12-23 00:13:18 -0800

    styled About component

--- water-my-plants/src/components/About.js
+++ water-my-plants/src/components/About.js
@@ -8,76 +8,92 @@ export default function About() {
       <div className='unit-container'>
         <h2>Unit 4</h2>
         <div className='team-member'>
-          <h3>
-            Arison Arias
-            <a href='https://github.com/Arison13'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Arison Arias</h3>
+            <h3>
+              <a href='https://github.com/Arison13'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/arias.png' alt='Profile pic of Arison Arias' />
-          <p>Backend Engineer</p>
+          <h5>Backend Engineer</h5>
         </div>
         <div className='team-member'>
-          <h3>
-            Jared Hall
-            <a href='https://github.com/Jaredhall18'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Jared Hall</h3>
+            <h3>
+              <a href='https://github.com/Jaredhall18'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/jared.png' alt='Profile pic of Jared Hall' />
-          <p>Backend Engineer</p>
+          <h5>Backend Engineer</h5>
         </div>
       </div>
 
       <div className='unit-container'>
         <h2>Unit 3</h2>
         <div className='team-member'>
-          <h3>
-            Jayson Byrd
-            <a href='https://github.com/ByrdJayson'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Jayson Byrd</h3>
+            <h3>
+              <a href='https://github.com/ByrdJayson'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/jayson.png' alt='Profile pic of Jayson Byrd' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
         <div className='team-member'>
-          <h3>
-            Lia Moua
-            <a href='https://github.com/moua0061'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Lia Moua</h3>
+            <h3>
+              <a href='https://github.com/moua0061'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/me.png' alt='Profile pic of Lia Moua' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
         <div className='team-member'>
-          <h3>
-            Ryan Howard
-            <a href='https://github.com/ryansnorek'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Ryan Howard</h3>
+            <h3>
+              <a href='https://github.com/ryansnorek'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/ryan.png' alt='Profile pic of Ryan Howard' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
       </div>
 
       <div className='unit-container'>
         <h2>Unit 2</h2>
         <div className='team-member'>
-          <h3>
-            Kayla Famurewa
-            <a href='https://github.com/kfam22'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Kayla Famurewa</h3>
+            <h3>
+              <a href='https://github.com/kfam22'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/kayla.png' alt='Profile pic of Kayla Famurewa' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
         <div className='team-member'>
-          <h3>
-            Brandon Booth
-            <a href='https://github.com/brandon-bee'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Brandon Booth</h3>
+            <h3>
+              <a href='https://github.com/brandon-bee'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/brandon.png' alt='Profile pic of Brandon Booth' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
         <div className='team-member'>
-          <h3>
-            Gage Milligan
-            <a href='https://github.com/gagemilligan'><FaGithub /></a>
-          </h3>
+          <div className='member-title'>
+            <h3>Gage Milligan</h3>
+            <h3>
+              <a href='https://github.com/gagemilligan'><FaGithub className='github-icon' /></a>
+            </h3>
+          </div>
           <img src='../Assets/team/gage.png' alt='Profile pic of Gage Milligan' />
-          <p>Frontend React Engineer</p>
+          <h5>Frontend React Engineer</h5>
         </div>
       </div>
     </div>
