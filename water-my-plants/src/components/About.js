27cdb3a34d06234e01f7814a97c0f8889b2430e9
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-12-22 23:29:27 -0800

    Created About component basic structure

---
+++ water-my-plants/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='about-container'>
+      <div className='unit-container'>
+        <h2>Unit 4</h2>
+        <div className='team-member'>
+          <h3>Arison Arias</h3>
+          <p>Backend Engineer</p>
+          <a href='https://github.com/Arison13'>Github Profile</a>
+        </div>
+        <div className='team-member'>
+          <h3>Jared Hall</h3>
+          <p>Backend Engineer</p>
+          <a href='https://github.com/Jaredhall18'>Github Profile</a>
+        </div>
+      </div>
+    </div>
+  )
+}
